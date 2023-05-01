f6abd8e5b618d5a7d7b854311a1101c908d13f50
thiagopfs <thiagopfs@gmail.com>
2023-05-01 18:46:15 -0300

    corrigindo o app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("rodando o sistema de gerenciamento de jogos")
-console.log("Mais com console de teste")
+console.log("rodando o sistema de gerenciamento de jogos");
+console.log("Mais com console de teste");
+console.log("Criada a pagina de contato");
