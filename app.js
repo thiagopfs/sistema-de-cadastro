58aadd45dd1f0f5726667a7053cb069bb7a2545d
thiagopfs <thiagopfs@gmail.com>
2023-05-01 18:17:14 -0300

    Testando o git restore

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
-console.log("rodando o sistema de gerenciamento de jogos");
-console.log("Mais com console de teste");
-console.log("push para o github sem passar url");
+console.log("rodando o sistema de gerenciamento de jogos")
+console.log("Mais com console de teste")
